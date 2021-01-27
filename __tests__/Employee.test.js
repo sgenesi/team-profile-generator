30378352bc4459b310fa991cd780575778a2257f
Staci Genesi <sgenesi@hotmail.com>
2021-01-26 20:31:07 -0700

    created employee classes and methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,7 +4,7 @@ const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
     const employee = new Employee();
-    expect(typeOf(employee)).toBe('object');
+    expect(typeof (employee)).toBe('object');
 });
 
 test('sets employee name', () => {
