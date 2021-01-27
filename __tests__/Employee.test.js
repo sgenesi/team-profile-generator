486629224717b80091cff0b339fb8445b9e7c58d
Staci Genesi <sgenesi@hotmail.com>
2021-01-26 20:28:53 -0700

    added tests to test Employee functions

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -15,13 +15,36 @@ test('sets employee name', () => {
 
 test('sets employee id', () => {
     const idValue = 50;
-    const employee = new Employee(id);
+    const employee = new Employee("Joe", idValue);
     expect(employee.id).toBe(idValue);
 });
 
 test('sets employee email address', () => {
     const emailAddress = "email@email.com";
-    const employee = new Employee(emailAddress);
+    const employee = new Employee("Joe", 50, emailAddress);
     expect(employee.email).toBe(emailAddress);
 });
 
+test('get name using getName()', () => {
+    const testName = 'Joe';
+    const employee = new Employee(testName);
+    expect(employee.getName()).toBe(testName);
+});
+
+test('get id using getId()', () => {
+    const testId = 50;
+    const employee = new Employee("Joe", testId);
+    expect(employee.getId()).toBe(testId);
+});
+
+test('get email using getEmail()', () => {
+    const testEmail = "email@email.com";
+    const employee = new Employee("Joe", 50, testEmail);
+    expect(employee.getEmail()).toBe(testEmail);
+});
+
+test('get role using getRole()', () => {
+    const testRole = "Employee";
+    const employee = new Employee('Joe', 50, "email@email.com");
+    expect(employee.getRole()).toBe(testRole);
+});
