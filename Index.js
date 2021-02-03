c629ec724bbcab0e4e8550103aed1fba73696a7c
Staci Genesi <sgenesi@hotmail.com>
2021-02-02 21:15:35 -0700

    added HTML and styles

---
+++ Index.js
@@ -0,0 +1,188 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const employees = [];
+
+function initApp() {
+    startHtml();
+    addMember();
+}
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office number";
+            }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "moreMembers"
+            }])
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    addHtml(newMember)
+                        .then(function () {
+                            if (moreMembers === "yes") {
+                                addMember();
+                            } else {
+                                finishHtml();
+                            }
+                        });
+
+                });
+        });
+}
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>My Team</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <style>
+            body { margin-bottom: 2em; }
+            .card { margin-bottom: 2em; }
+        </style>
+    </head>
+    <body>
+    <div class="jumbotron p-3 mb-2 bg-danger text-white text-center"><h1>My Team</h1></div>
+<div class="container">
+    <div class="row">`;
+    fs.writeFile("./dist/index.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
+function addHtml(member) {
+    return new Promise(function (resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const github = member.getGithub();
+            data = `<div class="row">
+            <div class="col">
+            <div class="card">
+                <div class="card-header p-3 mb-2 bg-primary text-white">
+                    <h2 class="card-header">${name}</h2>
+                    <h3><i class="fas fa-glasses"></i> Engineer</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">GitHub: ${github}</li>
+                </ul>
+            </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="row">
+            <div class="col">
+            <div class="card">
+                <div class="card-header p-3 mb-2 bg-primary text-white">
+                    <h2 class="card-header">${name}</h2>
+                    <h3><i class="fas fa-user-graduate"></i> Intern</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">School: ${school}</li>
+                </ul>
+            </div>`;
+        } else {
+            const officeNumber = member.getOfficeNumber();
+            data = `<div class="row">
+            <div class="col">
+            <div class="card">
+                <div class="card-header p-3 mb-2 bg-primary text-white">
+                    <h2 class="card-header">${name}</h2>
+                    <h3><i class="fas fa-mug-hot"></i> Manager</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">Office Number: ${officeNumber}</li>
+                </ul>
+            </div>`
+        }
+        console.log("adding team member");
+        fs.appendFile("./dist/index.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+}
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./dist/index.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("end");
+}
+
+initApp();
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # team-profile-generator
+
+# portfolio-generator
+The Portfolio Generator is a command-line application that dynamically generates a professional portfolio from a user's input using the Inquirer package.  
+
+Table of Contents
+* [User Story and Acceptance Criteria](#user-story-and-acceptance-criteria)
+*[Built With](#built-with)
+*[Key Features](#key-features)
+*[Links](#links)
+*[Images](#images)
+
+---
+## User Story and Acceptance Criteria
+
+
+### User Story
+
+```
+AS A web developer who needs a professional portfolio
+I WANT to be able to enter my information to generate a portfolio
+SO THAT I can spend more time coding new projects.
+```
+
+### Acceptance Criteria
+
+```
+WHEN I open the project on GitHub
+THEN I can clone the code to my computer.
+```
+```
+WHEN I open the code on my computer
+THEN I can run the node app.
+```
+
+```
+WHEN I run the app
+THEN I can enter my information.
+```
+```
+WHEN I open the page
+THEN I can see my professional portfolio.
+```
+
+---
+## Built With
+* Node.js
+* HTML
+* CSS
+
+---
+## Key Features
+* Allows user to input personal information and project information
+* Generates a professional portfolio
+
+---
+## Links
+GitHub Repo: https://github.com/sgenesi/portfolio-generator
+
+---
+## Images
+
+### VS Code Image
+![image](https://user-images.githubusercontent.com/71858457/104823411-18432100-5807-11eb-89ca-9e4b092a3bed.png)
+
+### Desktop Images of Generated Portfolio
+![image](https://user-images.githubusercontent.com/71858457/104823427-2bee8780-5807-11eb-9d61-08a7fca4a991.png)
+
+![image](https://user-images.githubusercontent.com/71858457/104823436-3ad53a00-5807-11eb-8886-9d26c2751776.png)
+
+
